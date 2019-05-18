200c5596b22cd19e849c76fc094adba800364169
linghuc <626185608@qq.com>
2019-05-19 00:31:16 +0800

    edit hot_fix2

--- src/main/java/com/qq/gitTestH/Fjbbvc.java
+++ src/main/java/com/qq/gitTestH/Fjbbvc.java
@@ -6,8 +6,12 @@ public class Fjbbvc {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello+xgz ---right-----");
-		System.out.println("hello+xgz---left---");
-	
+		System.out.println("hello+xgz---left--- hotfix");
+		
 	}
+	public String ff() {
+		return "xxs";
+	}
+
 
 }
